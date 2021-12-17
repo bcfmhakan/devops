cd76e40abdbf0809032c01d39106dfb67a58c673
hasan28 <hasan@bestcloudfor.me>
2021-12-17 12:59:39 +0300

    yaml editor added

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # devops
+
+Python YAML Editor
+
+pip install -r requirements.txt
+
+python3 yaml_editor.py --tag=<INT> --path=<STR>
---
+++ yaml_editor.py
@@ -0,0 +1,18 @@
+import yaml
+import argparse
+
+parser = argparse.ArgumentParser()
+path = parser.add_argument("-path", "--path", help="YAML Path - String", type=str)
+tag = parser.add_argument("-tag", "--tag", help="Tag Value - Integer", type=int)
+args = parser.parse_args()
+
+with open(f"{args.path}", "r") as file:
+    try:
+        f = yaml.safe_load(file)
+    except yaml.YAMLError as exc:
+        print(exc)
+
+f["deploy"]["tag"] = args.tag
+
+with open(f"{args.path}", "w") as output:
+    yaml.dump(f, output)
